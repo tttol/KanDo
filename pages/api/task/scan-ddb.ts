f1f37df4d040797828ede1854d831d54158368f0
t-takahashi <tttol.t4k.b1z@gmail.com>
2023-09-01 23:01:45 +0900

    add res.end()

--- pages/api/task/scan-ddb.ts
+++ pages/api/task/scan-ddb.ts
@@ -1,8 +1,9 @@
   import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+import { NextApiRequest, NextApiResponse } from "next";
 
   const client = new DynamoDBClient({});
 
-  const scanDdb = async () => {
+  const scanDdb = async (req: NextApiRequest, res: NextApiResponse) => {
     console.log("scna-ddb called!");
     try {
       const command = new ScanCommand({
@@ -14,7 +15,8 @@
       response.Items.forEach(function (data) {
         console.log(JSON.stringify(data));  // データ形式に注意
       });
-      return response;
+      // return response;
+      res.end();
     } catch (error) {
       console.error("An error occurred:", error);
     }
